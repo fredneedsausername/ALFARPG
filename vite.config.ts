5674d0b6bee8c808eb1767024bfc7bbcb92c294d
fred ver <fred.ver06@gmail.com>
2025-04-28 03:10:22 +0200

    Set up project for vite and licenses script

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: '.',
+    base: './',
+    build: {
+        outDir: 'dist', // Where to put the final build
+        emptyOutDir: true, // Clean 'dist' before each build
+    },
+    server: {
+        open: true // Auto-open the browser on npm run dev
+    },
+    preview: {
+        open: true, // Auto-open browser when running npm run preview
+    },
+})
