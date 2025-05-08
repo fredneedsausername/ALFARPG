db4e79d22d1b90831191ba4622a71ddcba892708
fred ver <fred.ver06@gmail.com>
2025-05-09 00:59:37 +0200

    Added configuration for game

--- src/config/game.ts
+++ src/config/game.ts
@@ -1,5 +1,41 @@
 import Phaser from 'phaser'
 
-class ALFARPG extends Phaser.Game {
+export class ALFARPG extends Phaser.Game {
     
+    constructor(callbackFunctions: Phaser.Types.Core.CallbacksConfig,
+        ...scenes: Phaser.Types.Scenes.SettingsConfig[]) {
+
+        const gameConfig: Phaser.Types.Core.GameConfig = {
+            "disableContextMenu": true,
+            scene: scenes,
+            callbacks: callbackFunctions,
+            physics: {
+                default: "arcade",
+                arcade: {
+                    fps: 300,
+                    fixedStep: false,
+                    width: 1920,
+                    height: 1080,
+                    gravity: {
+                        x: 0,
+                        y: 0
+                    }
+                },
+            },
+            loader: {
+                imageLoadType: "HTMLImageElement",
+                path: "/assets/"
+            },
+            scale: {
+                mode: Phaser.Scale.FIT,
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+            },
+            render: {
+                pixelArt: true // Essential for proper sprite scaling on different screen resolutions
+            }
+        }
+
+        super(gameConfig)
+
+    }
 }
