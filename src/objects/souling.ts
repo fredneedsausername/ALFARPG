65430c5ac86bbfdae048ba339844451006a5bbb3
fred ver <fred.ver06@gmail.com>
2025-05-12 21:36:11 +0200

    Cleaned up souling.ts

--- src/objects/souling.ts
+++ src/objects/souling.ts
@@ -1,13 +1,3 @@
 class Souling extends Phaser.Physics.Arcade.Sprite {
     
 }
-
-
-
-
-Primo Pet
-Terreno Erboso
-Terreno Desertico
-Terreno Innevato
-Distributore Gacha Premium
-Distributore Gacha
